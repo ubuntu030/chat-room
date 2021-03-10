f2bcc7c841d520c069087dfc58fb6580a62304e0
ubuntu030 <ubuntu030@gmail.com>
2021-03-10 15:46:25 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./App.scss";
+
+const App = () => {
+	return (
+		<div className="app-container">
+			<div className="opration-area">
+				
+			</div>
+			<div className="present-area">
+
+			</div>
+		</div>
+	)
+}
+
+export default App;
