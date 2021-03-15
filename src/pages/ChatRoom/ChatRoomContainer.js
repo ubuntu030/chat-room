bd3c4211b57d74c61cee2dcfe9936177fe5d87e7
ubuntu030 <ubuntu030@gmail.com>
2021-03-15 17:14:54 +0800

    connect to socket server

--- src/pages/ChatRoom/ChatRoomContainer.js
+++ src/pages/ChatRoom/ChatRoomContainer.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+
 import ChatRoomComponent from "./components/ChatRoomComponent";
 
 import messages from "../../../src/api/chat_data.json";
 
+
+
 const ChatRoomContainer = () => {
-	const props = {
-		messages
-	}
+	useEffect(() => {
+		const socket = io("http://localhost:3030", {
+			reconnectionDelayMax: 10000,
+		});
+	}, []);
+
 	return (
 		<>
 			<ChatRoomComponent messages={messages} />
